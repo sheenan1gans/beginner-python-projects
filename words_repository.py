2f7febddb48bb226809b381afe02e2882f4a6083
sheenan1gans <28harsheen@gmail.com>
2025-09-21 12:26:31 +0530

    Word scramble game

---
+++ words_repository.py
@@ -0,0 +1,33 @@
+# word bank with categories (easy to extend)
+words = {
+    "fruits": [
+        "apple", "banana", "orange", "grape", "pineapple", "mango", "papaya",
+        "watermelon", "kiwi", "strawberry", "blueberry", "blackberry", "coconut",
+        "cherry", "pomegranate", "peach", "plum", "pear", "apricot", "fig"
+    ],
+
+    "animals": [
+        "lion", "tiger", "elephant", "giraffe", "zebra", "cheetah", "kangaroo",
+        "panda", "monkey", "dolphin", "shark", "whale", "penguin", "ostrich",
+        "alligator", "crocodile", "rabbit", "squirrel", "buffalo", "camel"
+    ],
+
+    "countries": [
+        "india", "canada", "brazil", "germany", "france", "italy", "spain",
+        "china", "japan", "korea", "australia", "egypt", "russia", "mexico",
+        "argentina", "chile", "turkey", "greece", "sweden", "norway"
+    ],
+
+    "colours": [
+        "red", "blue", "green", "yellow", "purple", "pink", "orange", "brown",
+        "black", "white", "gray", "violet", "indigo", "gold", "silver",
+        "beige", "maroon", "turquoise", "teal", "navy"
+    ],
+
+    "sports": [
+        "cricket", "football", "hockey", "tennis", "badminton", "basketball",
+        "volleyball", "golf", "rugby", "cycling", "swimming", "boxing", "karate",
+        "archery", "wrestling", "skiing", "surfing", "skating", "rowing", "baseball"
+    ]
+
+}
